607297b0a9095dfd6180cb5c091e3b2194efaf02
Raivis <raivukam2@gmail.com>
2024-11-11 03:12:45 +0200

    validation and a test connection endpoint

--- src/main/java/com/alphabot/telegram/message/controller/TelegramController.java
+++ src/main/java/com/alphabot/telegram/message/controller/TelegramController.java
@@ -5,6 +5,7 @@ import com.alphabot.telegram.message.telegram.*;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.*;
+import org.springframework.util.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,26 +24,39 @@ public class TelegramController {
                 Received sendTextMessage with:\s
                 telegram Id: {}
                 message: {}""", messageDTO.getTelegramId(), messageDTO.getMessage());
+
+        if (messageDTO.getTelegramId() == null)
+            return ResponseEntity.badRequest().body("telegramId is null");
+
+        if (StringUtils.hasText(messageDTO.getMessage()))
+            return ResponseEntity.badRequest().body("message is empty");
+
         telegramBot.sendTextMessage(messageDTO.getTelegramId(), messageDTO.getMessage());
 
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping(consumes ="application/json")
-    @RequestMapping("/sendMessageWithButtons")
-    public ResponseEntity<?> sendMessageWithButtons(@RequestBody MessageDTO messageDTO) {
-
-        telegramBot.sendMessageWithButtons(messageDTO.getTelegramId(), messageDTO.getMessage());
-
-        return ResponseEntity.ok().build();
+    @GetMapping
+    @RequestMapping("/test")
+    public ResponseEntity<?> testConnection() {
+        return ResponseEntity.ok("Test successful");
     }
 
-    @PostMapping(consumes ="application/json")
-    @RequestMapping("/sendMessageWithInputField")
-    public ResponseEntity<?> sendMessageWithInputField(@RequestBody MessageDTO messageDTO) {
-
-        telegramBot.sendCustomInputKeyboard(messageDTO.getTelegramId());
-
-        return ResponseEntity.ok().build();
-    }
+//    @PostMapping(consumes ="application/json")
+//    @RequestMapping("/sendMessageWithButtons")
+//    public ResponseEntity<?> sendMessageWithButtons(@RequestBody MessageDTO messageDTO) {
+//
+//        telegramBot.sendMessageWithButtons(messageDTO.getTelegramId(), messageDTO.getMessage());
+//
+//        return ResponseEntity.ok().build();
+//    }
+//
+//    @PostMapping(consumes ="application/json")
+//    @RequestMapping("/sendMessageWithInputField")
+//    public ResponseEntity<?> sendMessageWithInputField(@RequestBody MessageDTO messageDTO) {
+//
+//        telegramBot.sendCustomInputKeyboard(messageDTO.getTelegramId());
+//
+//        return ResponseEntity.ok().build();
+//    }
 }
